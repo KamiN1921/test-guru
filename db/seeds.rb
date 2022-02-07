f27845b0b1b0aad81bc4b9009360272b8ad19ec6
Chaenkova Darya <d.chaenkova@madpie.org>
2022-02-07 21:27:19 +0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+  User.create([{ login: "Vasya" }, { login: "Oleg" }])
+  Category.create([{title: "General"},{title: "Programming"}, {title:"Database"}])
+  Test.create([{title: "HTML",category_id:"1"},{title:"Ruby", level: "1", category_id:"2"}])
+  Question.create([{body: "First", test_id:"1"},{body: "Second", test_id: "2"}])
+  Answer.create([{body:"Yes",question_id:"1"},{body:"No", correct: false, question_id: "1"}])
