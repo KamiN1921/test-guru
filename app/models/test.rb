b5dbbf925b69465b78f860d89fb8f9786d61825c
Chaenkova Darya <d.chaenkova@madpie.org>
2022-02-10 23:38:25 +0300

    new methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,8 @@
 class Test < ApplicationRecord
+  has_many :results
+  belongs_to :category
 
+  def self.test_from_category(category)
+    select("title").from("tests").joins("JOIN categories on tests.category_id = categories.id WHERE categories.title = '#{category}'").order(title: "DESC")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
   User.create([{ login: "Vasya" }, { login: "Oleg" }])
   Category.create([{title: "General"},{title: "Programming"}, {title:"Database"}])
-  Test.create([{title: "HTML",category_id:"1"},{title:"Ruby", level: "1", category_id:"2"}])
+  Test.create([{title: "HTML", category_id:"1"},{title:"Ruby", level: "1", category_id:"2"}])
   Question.create([{body: "First", test_id:"1"},{body: "Second", test_id: "2"}])
   Answer.create([{body:"Yes",question_id:"1"},{body:"No", correct: false, question_id: "1"}])
+  Result.create([{user_id:"1", test_id:"1" }, {user_id:"1", test_id:"2" }, {user_id:"1", test_id:"3" }])
