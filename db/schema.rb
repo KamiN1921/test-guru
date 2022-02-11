23fffb852efe3fe81974ead343caf086e8182340
Chaenkova Darya <d.chaenkova@madpie.org>
2022-02-11 13:05:41 +0300

    Замена select на pluck

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,11 @@ ActiveRecord::Schema.define(version: 2022_02_08_170228) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "question_id"
     t.boolean "correct", default: true
+    t.integer "questions_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["questions_id"], name: "index_answers_on_questions_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -28,28 +29,29 @@ ActiveRecord::Schema.define(version: 2022_02_08_170228) do
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "test_id"
-    t.integer "answer_id"
+    t.integer "tests_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["tests_id"], name: "index_questions_on_tests_id"
   end
 
   create_table "results", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "test_id", null: false
+    t.integer "users_id", null: false
+    t.integer "tests_id", null: false
     t.boolean "clear", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_results_on_test_id"
-    t.index ["user_id"], name: "index_results_on_user_id"
+    t.index ["tests_id"], name: "index_results_on_tests_id"
+    t.index ["users_id"], name: "index_results_on_users_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
-    t.integer "category_id"
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,9 +60,9 @@ ActiveRecord::Schema.define(version: 2022_02_08_170228) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "questions", "answers"
-  add_foreign_key "questions", "tests"
-  add_foreign_key "results", "tests"
-  add_foreign_key "results", "users"
+  add_foreign_key "answers", "questions", column: "questions_id"
+  add_foreign_key "questions", "tests", column: "tests_id"
+  add_foreign_key "results", "tests", column: "tests_id"
+  add_foreign_key "results", "users", column: "users_id"
   add_foreign_key "tests", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
   User.create([{ login: "Vasya" }, { login: "Oleg" }])
   Category.create([{title: "General"},{title: "Programming"}, {title:"Database"}])
-  Test.create([{title: "HTML", category_id:"1"},{title:"Ruby", level: "1", category_id:"2"}])
-  Question.create([{body: "First", test_id:"1"},{body: "Second", test_id: "2"}])
-  Answer.create([{body:"Yes",question_id:"1"},{body:"No", correct: false, question_id: "1"}])
-  Result.create([{user_id:"1", test_id:"1" }, {user_id:"1", test_id:"2" }, {user_id:"1", test_id:"3" }])
+  Test.create([{title: "HTML", category_id: Category.first.id},{title:"Ruby", level: "1", category_id:Category.last.id}])
+  Question.create([{body: "First", test_id:Test.first.id},{body: "Second", test_id: Test.last.id}])
+  Answer.create([{body:"Yes",question_id:Question.first.id},{body:"No", correct: false, question_id: Question.last.id}])
+  Result.create([{user_id:User.first.id, test_id:Test.first.id }, {user_id:User.last.id, test_id:Test.first.id }, {user_id:User.first.id, test_id:Test.last.id }])
