f4e73172d7c2522cefa0e844b8e37f33db3498f2
Chaenkova Darya <d.chaenkova@madpie.org>
2022-02-21 22:13:19 +0300

    fix uniq, fix scopes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,8 +6,7 @@ class Answer < ApplicationRecord
   validate :more_than_four
 
   def more_than_four
-    mass = Answer.where(question_id: self.question_id).pluck
-    if mass.length >= 4
+    if question.answers.count >= 4
       errors.add(:more_than_four, "can't be more than 4")
     end
   end
--- app/models/test.rb
+++ app/models/test.rb
@@ -4,12 +4,16 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User"
 
-  scope :test_from_category, ->(category) {joins(:category).where('categories.title = ?',category).order(title: "DESC").pluck("title")}
+  scope :test_categories, -> {joins(:category)}
   scope :easy, -> {where(level: 0..1)}
   scope :medium, -> {where(level: 2..4)}
   scope :hard, ->{where("level>5")}
 
   validates :level, numericality: { only_integer: true }
-  validates :body, presence: true
-  validates :title, :level, uniqueness: true
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: :level, message: "must be uniq in level"}
+
+  def self.test_from_category(category)
+    test_categories.where('categories.title = ?',category).order(title: "DESC").pluck("title")
+  end
 end
