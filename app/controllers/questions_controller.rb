9aca5a257ecc7768ba68a9d6f0c795481282d863
Chaenkova Darya <d.chaenkova@madpie.org>
2022-03-12 16:15:20 +0300

    add exception

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,8 @@ class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index new create]
   before_action :find_question, only: %i[show edit update destroy]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     @questions = @test.questions
   end
@@ -37,4 +39,7 @@ class QuestionsController < ApplicationController
     @test = Test.find(params[:test_id])
   end
 
+  def rescue_with_question_not_found
+    render plain: "Question not found"
+  end
 end
