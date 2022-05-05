8541c2a303a06b890d48f4dda76d48633e13d44a
Chaenkova Darya <d.chaenkova@madpie.org>
2022-05-05 16:19:47 +0300

    fix

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,8 +3,7 @@ class Result < ApplicationRecord
 
   belongs_to :user
   belongs_to :test
-  belongs_to :current_question, class_name: 'Question', optional: true, dependent: :delete
-
+  belongs_to :current_question, class_name: 'Question', optional: true
   before_validation :before_validation_set_first, on: :create
   before_update :next_question
 
