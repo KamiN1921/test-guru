1f30b8e73dbc5c74f4bf1418492256aaac71b251
Chaenkova Darya <d.chaenkova@madpie.org>
2022-05-05 14:53:40 +0300

    fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,6 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   validates :body, presence: true
   belongs_to :test
-  has_many :results, foreign_key: 'current_question_id', dependent: :destroy
-  has_many :gists, dependent: :restrict_with_exception
+  has_many :results, foreign_key: 'current_question_id', dependent: :delete_all
+  has_many :gists, dependent: :delete_all
 end
